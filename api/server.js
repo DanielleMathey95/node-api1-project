d6b57f4ff4ec42ba7aaafbcaaad1cb6af80a17d7
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-09-01 17:39:15 -0700

    set up CRUD endpoints

--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
 //IMPORTS AT THE TOP
-const express = require(express);
+const express = require("express");
 
 //INSTANCE OF THE EXPRESS APP
 const server = express();
@@ -8,9 +8,19 @@ const server = express();
 server.use(express.json());
 
 //ENDPOINTS req is talking to the server, res is the response given to the client
-server.use("*", (req, res) => {
+server.use("/", (req, res) => {
   res.status(200).json({message: "Why hello there!"})
 });
 
+// [GET] api/users/:id (R of CRUD, fetch user by id)
 
-module.exports = server; // EXPORT YOUR SERVER instead of {}
+// [GET] api/users (R of CRUD, fetch all users)
+
+// [POST] api/users (C of CRUD, create new user from JSON payload)
+
+// [PUT] api/users/:id (U of CRUD, update user with :id using JSON payload)
+
+// [DELETE] api/users/:id (D of CRUD, remove user with :id)
+
+
+module.exports = server;
