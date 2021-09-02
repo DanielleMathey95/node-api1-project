d4cd8a2bd7b1dfc0693dfa28e0113ce3d19cedfd
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-09-01 18:25:06 -0700

    Completed both GET requests and POST request, tested all three and they work fine

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 //IMPORTS AT THE TOP
 const express = require("express");
+const User = require("./users/model");
 
 //INSTANCE OF THE EXPRESS APP
 const server = express();
@@ -8,19 +9,63 @@ const server = express();
 server.use(express.json());
 
 //ENDPOINTS req is talking to the server, res is the response given to the client
-server.use("/", (req, res) => {
-  res.status(200).json({message: "Why hello there!"})
-});
 
 // [GET] api/users/:id (R of CRUD, fetch user by id)
 
+server.get("/api/users/:id", (req, res) => {
+  const userId = req.params.id;
+  User.findById(userId)
+    .then((users) => {
+      if (!users) {
+        res.status(404).json("User not found");
+      } else {
+        res.status(200).json(users);
+      }
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ message: "Could not get user by id", error: error.message });
+    });
+});
+
 // [GET] api/users (R of CRUD, fetch all users)
 
+server.get("/api/users", (req, res) => {
+  User.find()
+    .then((users) => {
+      // console.log(users)
+      res.status(200).json(users);
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ message: "Could not get all users", error: error.message });
+    });
+});
+
 // [POST] api/users (C of CRUD, create new user from JSON payload)
+
+server.post("/api/users", (req, res) => {
+  const newUser = req.body;
+  User.insert(newUser)
+    .then(users => {
+      res.status(201).json(users)
+    })
+    .catch(error => {
+      res.status(500).json({message: "Could not post new user", error: error.message})
+    })
+});
 
 // [PUT] api/users/:id (U of CRUD, update user with :id using JSON payload)
 
 // [DELETE] api/users/:id (D of CRUD, remove user with :id)
 
+
+
+
+server.use("*", (req, res) => {
+  res.status(404).json({ message: "Could not access" });
+});
 
 module.exports = server;
